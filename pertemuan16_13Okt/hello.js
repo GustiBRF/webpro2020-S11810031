bc49bb9f16c41ba4f7409c9631b93be8203548b0
GustiBRF <gustibagusrf@gmail.com>
2020-10-13 14:42:41 -0700

    1st commit

---
+++ pertemuan16_13Okt/hello.js
@@ -0,0 +1,10 @@
+
+export const hello = ( user) => {
+
+    console.log(`Hello, ${user}`);
+}
+
+export const number = [1,2,3,4,5,6,7];
+
+//export default hello;
+
---
+++ pertemuan16_13Okt/index.js
@@ -0,0 +1,6 @@
+import { hello } from './component/world.js';
+
+//impors * as helper from "./hello.js"
+
+hello ('John Doe');
+//console.log(number)
